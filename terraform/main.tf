4ce5609f3e7da1fb877c101d7a64fac6f89456a3
sshivanijoshi <38161428+sshivanijoshi@users.noreply.github.com>
2023-02-17 10:16:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,15 @@
 terraform {
-
-   required_version = ">=0.12"
-   
-   backend "azurerm" {
-    resource_group_name  = "Shivani-RG"
-    storage_account_name = "shivanistorage12"
-    container_name       = "tfstate"
-    key                  = "tfsj"
-  }
-
-   required_providers {
+required_providers {
      azurerm = {
        source = "hashicorp/azurerm"
        version = "~>2.0"
      }
    }
  }
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
